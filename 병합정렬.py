6477260832db40c958cc187c8ba4cb6a99c65812
woqls8449 <woqls8449@gmail.com>
2020-05-29 00:56:35 +0900

    분할 정복 | 병합 정렬 (Merge Sort)

---
+++ 병합정렬.py
@@ -0,0 +1,26 @@
+# 병합 정렬 (Merge Sort)
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+    middle = len(arr) // 2
+    left = [:middle]
+    right = [middle:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    while len(left) > 0 or len(right) > 0:
+        if len(left) > 0 and len(right) > 0:
+            if left[0] <= right[0]:
+                result.append(left.pop(0))
+            else:
+                result.append(right.pop(0))
+        elif len(left) > 0:
+            result.append(left.pop(0))
+        elif len(right) > 0:
+            result.append(right.pop(0))
+    return result
